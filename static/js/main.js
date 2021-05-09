db86aa09ca2d14f3c3b671a2d4bd41e672946d77
Travis Taylor <travisdoesmath@gmail.com>
2021-05-08 22:32:33 -0500

    added line indicators and changed to dark mode

--- static/js/main.js
+++ static/js/main.js
@@ -1,8 +1,22 @@
 let globalValues = {};
 
+function getLines(d) {
+    let lines = [];
+    if (d.BLUE) lines.push('#00a1de')
+    if (d.RED) lines.push('#c60c30')
+    if (d.BRN) lines.push('#62361b')
+    if (d.G) lines.push('#009b3a')
+    if (d.O) lines.push('#f9461c')
+    if (d.P || d.Pexp) lines.push('#522398')
+    if (d.Pnk) lines.push('#e27ea6')
+    if (d.Y) lines.push('#f9e300')
+
+    return lines;
+}
+
 let tableConfig = {
-    'width': 640,
-    'height': 300,
+    'width': 600,
+    'height': 400,
     'sort': {
         'column':'rides-distribution',
         'direction': 'descending'
@@ -15,35 +29,43 @@ let tableConfig = {
         {
             'id': 'station-name',
             'name': 'Name',
-            'type': 'string',
-            'data': x => x.name,
-            'width': 140
+            'type': 'indicators',
+            'data': x => {
+                let lines = getLines(x)
+                return {
+                    'name': x.name,
+                    'indicators': lines
+                }
+            },
+            'width': 160
         },
         {
             'id': 'rides-distribution',
-            'name': 'Rides Dist.',
+            'name': 'Daily Rides Dist. & Average',
             'type': 'spark-boxplot',
             'data': x => { 
                 return {
                     'q1': x.rides_q1,
                     'median': x.rides_median,
+                    'mean': x.rides_mean,
                     'q3': x.rides_q3,
                 }
             },
-            'width': 80,
+            'labelFormat': x => x.toLocaleString(undefined, {maximumFractionDigits: 0}),
+            'width': 120,
             'sortComparators': {
                 'ascending': (a, b) => a.rides_median - b.rides_median,
                 'descending': (a, b) => b.rides_median - a.rides_median,
             }
         },
-        {
-            'id': 'average-rides',
-            'name': 'Avg. Daily Rides',
-            'type': 'hbar',
-            'data': x => x.rides_mean,
-            'width': 64,
-            'labelFormat': x => x.toLocaleString(undefined, {maximumFractionDigits: 0})
-        },
+        // {
+        //     'id': 'average-rides',
+        //     'name': 'Avg. Daily Rides',
+        //     'type': 'hbar',
+        //     'data': x => x.rides_mean,
+        //     'width': 128,
+        //     'labelFormat': x => x.toLocaleString(undefined, {maximumFractionDigits: 0})
+        // },
         // {
         //     'id': 'average-rides',
         //     'name': 'Avg. Rides',
@@ -105,8 +127,8 @@ let tableConfig = {
 function parseRow(row) {
     let displayString = x => x;
     let displayDate = (d, format) => format(d);
-    let displaySparkBoxplot = function(d, width) {
-        let height = 17;
+    let displaySparkBoxplot = function(d, width, format) {
+        let height = 32;
 
         let xScale = d3.scaleLinear()
             .domain([globalValues.minRide, globalValues.maxRide])
@@ -118,12 +140,16 @@ function parseRow(row) {
 
         let outputString = `<svg width="${width}" height="${height}" style="shape-rendering: crispEdges">`
         outputString += `<g transform="translate(1,0)">`
-        outputString += `<line x1=${xScale(whiskerLow)} y1=${0.5*height} x2=${xScale(whiskerHigh)} y2=${0.5*height} style="stroke: black"></line>`
-        outputString += `<rect x=${xScale(d.q1)} y=0 height=${height} width=${xScale(d.q3 - d.q1)} fill="#F55"></rect>`
-        outputString += `<line x1=${xScale(whiskerLow)} y1=${0.25*height} x2=${xScale(whiskerLow)} y2=${0.75*height} style="stroke: black"></line>`
-        outputString += `<line x1=${xScale(d.median)} y1=0 x2=${xScale(d.median)} y2=${height} style="stroke: #FFF"></line>`
-        outputString += `<line x1=${xScale(whiskerHigh)} y1=${0.25*height} x2=${xScale(whiskerHigh)} y2=${0.75*height} style="stroke: black"></line>`
+        outputString += `<line x1=${xScale(whiskerLow)} y1=${0.25*height} x2=${xScale(whiskerHigh)} y2=${0.25*height} style="stroke: white"></line>`
+        outputString += `<rect x=${xScale(d.q1)} y=${0.0625*height} height=${0.375*height} width=${xScale(d.q3 - d.q1)} fill="#c60c30"></rect>`
+        outputString += `<line x1=${xScale(whiskerLow)} y1=${0.125*height} x2=${xScale(whiskerLow)} y2=${0.375*height} style="stroke: white"></line>`
+        outputString += `<line x1=${xScale(d.median)} y1=${0.0625*height} x2=${xScale(d.median)} y2=${0.4375*height} style="stroke: #FFF"></line>`
+        outputString += `<line x1=${xScale(whiskerHigh)} y1=${0.125*height} x2=${xScale(whiskerHigh)} y2=${0.375*height} style="stroke: white"></line>`
+
+        outputString += `<rect x=0 y="${0.625*height}" width="${xScale(d.mean)}" height="${0.375*height}" fill="#00a1de"></rect>`
+        outputString += `<text x="${xScale(d.mean)}" y="${0.75*height}" dx="5" dy="7" class="data-label" fill="white">${format(d.mean)}</text>`
         
+
         outputString += '</g></svg>'
 
         return outputString
@@ -138,9 +164,9 @@ function parseRow(row) {
 
         let outputString = `<svg width="${width}" height="${height}">`
         outputString += `<g transform="translate(1,0)">`
-        outputString += `<line x1=${xScale(d.start)} y1=${0.5*height} x2=${xScale(d.end)} y2=${0.5*height} style="stroke: black"></line>`
-        outputString += `<circle cx=${xScale(d.start)} cy=${0.5*height} r=${r} fill="${d.start - globalValues.minStart == 0 ? '#FFF' : '#000'}" stroke="black"></circle>`
-        outputString += `<circle cx=${xScale(d.end)} cy=${0.5*height} r=${r} fill="${d.end - globalValues.maxEnd == 0 ? '#FFF' : '#000'}" stroke="black"></circle>`
+        outputString += `<line x1=${xScale(d.start)} y1=${0.5*height} x2=${xScale(d.end)} y2=${0.5*height} style="stroke: #DDD"></line>`
+        outputString += `<circle cx=${xScale(d.start)} cy=${0.5*height} r=${r} fill="${d.start - globalValues.minStart == 0 ? '#DDD' : '#000'}" stroke="white"></circle>`
+        outputString += `<circle cx=${xScale(d.end)} cy=${0.5*height} r=${r} fill="${d.end - globalValues.maxEnd == 0 ? '#DDD' : '#000'}" stroke="white"></circle>`
 
         outputString += '</g></svg>'
 
@@ -163,10 +189,10 @@ function parseRow(row) {
             .range([height - 2*r, r])
 
         let outputString = `<svg width="${width}" height="${height}" style="shape-rendering: crispEdges; display: block; margin:0 auto;">`
-        outputString += `<rect x=0 y=0 width="${width}" height="${height}" fill="#DDD"></rect>`
-        outputString += `<line x1=${0.5*width} x2=${0.5*width} y1=0 y2=${height} style="stroke: white"></line>`
-        outputString += `<line x1=0 x2=${width} y1=${0.5*height} y2=${0.5*height} style="stroke: white"></line>`
-        outputString += `<rect x=${xScale(d.longitude) - r} y=${yScale(d.latitude) - r} width=${2*r} height=${2*r} fill="#555"></rect>`
+        outputString += `<rect x=0 y=0 width="${width}" height="${height}" fill="#555"></rect>`
+        outputString += `<line x1=${0.5*width} x2=${0.5*width} y1=0 y2=${height} style="stroke: #222"></line>`
+        outputString += `<line x1=0 x2=${width} y1=${0.5*height} y2=${0.5*height} style="stroke: #222"></line>`
+        outputString += `<rect x=${xScale(d.longitude) - r} y=${yScale(d.latitude) - r} width=${2*r} height=${2*r} fill="#BBB"></rect>`
         //outputString += `<circle cx=${xScale(d.longitude)} cy=${yScale(d.latitude)} r=${r} fill="#555"></circle>`
         outputString += '</svg>'
 
@@ -188,7 +214,7 @@ function parseRow(row) {
 
         let outputString = `<svg width="${width}" height="${height}" style="shape-rendering: crispEdges; display: block; margin:0 auto;">`
         d.forEach(x => {
-            outputString += `<rect x=${xScale(x.x)} y=${yScale(x.y)} width=${bandwidth} height=${height - yScale(x.y)} fill="#75b8d9"></rect>`
+            outputString += `<rect x=${xScale(x.x)} y=${yScale(x.y)} width=${bandwidth} height=${height - yScale(x.y)} fill="#888"></rect>`
         })
 
         outputString += '</svg>'
@@ -201,15 +227,30 @@ function parseRow(row) {
 
         let xScale = d3.scaleLinear()
             .domain([globalValues.minAverage, globalValues.maxAverage])
-            .range([0, width])
+            .range([0, 0.625*width])
 
         let outputString = `<svg width="${width}" height="${height}" style="shape-rendering: crispEdges; display: block; margin:0 auto;">`
-        outputString += `<rect x=0 y="${0.5*height}" width="${xScale(d)}" height="${0.5*height}" fill="#75b8d9"></rect>`
-        outputString += `<text x=0 y="${0.5*height}" dy="-5" class="data-label">${format(d)}</text>`
+        outputString += `<rect x=0 y="${0.25*height}" width="${xScale(d)}" height="${0.5*height}" fill="#75b8d9"></rect>`
+        outputString += `<text x="${xScale(d)}" y="${0.5*height}" dx="5" dy="5" class="data-label">${format(d)}</text>`
 
         outputString += '</svg>'
 
         return outputString;       
+    }
+    let displayIndicators = function(d, width) {
+        let height = 16;
+        let r = 7;
+
+        let outputString = `<div>${d.name}</div>`
+        if (d.indicators) {
+            outputString += `<svg width="${width}" height="${height}">`
+            d.indicators.forEach((d, i) => {
+                outputString += `<circle cx=${3*i*r + r} cy=${0.5*height} r=${r} fill=${d}></circle>`
+            })   
+            outputString += '</svg>'
+        }
+
+        return outputString;
     }
 
     let outputString = ''
@@ -217,12 +258,12 @@ function parseRow(row) {
         if (col.type == 'number')           outputString += `<div class="table-cell" style="width:${col.width}px"><div>${displayNumber(col.data(row), col.format)}</div></div>`;
         if (col.type == 'string')           outputString += `<div class="table-cell" style="width:${col.width}px"><div>${displayString(col.data(row))}</div></div>`;
         if (col.type == 'date')             outputString += `<div class="table-cell" style="width:${col.width}px"><div>${displayDate(col.data(row), col.format)}</div></div>`;
-        if (col.type == 'spark-boxplot')    outputString += `<div class="table-cell" style="width:${col.width}px"><div style="width:100%">${displaySparkBoxplot(col.data(row), col.width)}</div></div>`;
+        if (col.type == 'spark-boxplot')    outputString += `<div class="table-cell" style="width:${col.width}px"><div style="width:100%">${displaySparkBoxplot(col.data(row), col.width, col.labelFormat)}</div></div>`;
         if (col.type == 'spark-gantt')      outputString += `<div class="table-cell" style="width:${col.width}px"><div style="width:100%">${displaySparkGantt(col.data(row), col.width)}</div></div>`;
         if (col.type == 'spark-geo')        outputString += `<div class="table-cell" style="width:${col.width}px"><div style="width:100%">${displaySparkGeo(col.data(row), col.width)}</div></div>`;
         if (col.type == 'bar')              outputString += `<div class="table-cell" style="width:${col.width}px"><div style="width:100%">${displayBar(col.data(row), col.width)}</div></div>`;
         if (col.type == 'hbar')             outputString += `<div class="table-cell" style="width:${col.width}px"><div style="width:100%">${displayHBar(col.data(row), col.width, col.labelFormat)}</div></div>`;
-
+        if (col.type == 'indicators')       outputString += `<div class="table-cell" style="width:${col.width}px"><div style="width:100%">${displayIndicators(col.data(row), col.width)}</div></div>`;
     })
 
     return outputString;
